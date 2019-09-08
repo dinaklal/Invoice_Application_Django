92a69b598efb23e7667eee8c4f14bb64c45a88b5
dinaklal <dinaklal@gmail.com>
2019-09-08 22:43:21 +0530

    Databases 1

--- Invoice/login/views.py
+++ Invoice/login/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import User
 # Create your views here.
 def login(request):
+    u1= User()
     return render(request,'index.html',{'name':'Mughals Invoice'})
